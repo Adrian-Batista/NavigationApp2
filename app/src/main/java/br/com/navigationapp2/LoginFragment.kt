5839d35bba660fe9459585b7b82ab6b8853f4db9
mbegnini <mauricio1989@gmail.com>
2021-06-07 18:53:27 -0300

    animacoes e novo fragment

--- app/src/main/java/br/com/navigationapp2/LoginFragment.kt
+++ app/src/main/java/br/com/navigationapp2/LoginFragment.kt
@@ -1,6 +1,19 @@
 package br.com.navigationapp2
 
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 
 class LoginFragment: Fragment(R.layout.fragment_login) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val loginButton = getView()?.findViewById<Button>(R.id.confirmButton)
+        loginButton?.setOnClickListener {
+            val action = LoginFragmentDirections.actionLoginFragmentToWelcomeFragment()
+            findNavController().navigate(action)
+        }
+    }
 }
---
+++ app/src/main/java/br/com/navigationapp2/WelcomeFragment.kt
@@ -0,0 +1,6 @@
+package br.com.navigationapp2
+
+import androidx.fragment.app.Fragment
+
+class WelcomeFragment: Fragment(R.layout.fragment_welcome) {
+}
