6c9d387ced2d85e9ef6e9b2f5f015abbca5a335d
mbegnini <mauricio1989@gmail.com>
2021-06-07 19:46:45 -0300

    view binding e parametros

--- app/src/main/java/br/com/navigationapp2/LoginFragment.kt
+++ app/src/main/java/br/com/navigationapp2/LoginFragment.kt
@@ -1,18 +1,43 @@
 package br.com.navigationapp2
 
 import android.os.Bundle
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
 import android.widget.Button
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
+import br.com.navigationapp2.databinding.FragmentLoginBinding
 
 class LoginFragment: Fragment(R.layout.fragment_login) {
 
+    private var _binding: FragmentLoginBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentLoginBinding.inflate(inflater, container, false)
+        val view = binding.root
+        return view
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        val loginButton = getView()?.findViewById<Button>(R.id.confirmButton)
-        loginButton?.setOnClickListener {
-            val action = LoginFragmentDirections.actionLoginFragmentToWelcomeFragment()
+
+        binding.confirmButton.setOnClickListener {
+            val username = binding.usernameEditText.text.toString()
+            val password = binding.passwordEditText.text.toString()
+            val action = LoginFragmentDirections.actionLoginFragmentToWelcomeFragment(username, password)
             findNavController().navigate(action)
         }
     }
--- app/src/main/java/br/com/navigationapp2/WelcomeFragment.kt
+++ app/src/main/java/br/com/navigationapp2/WelcomeFragment.kt
@@ -1,6 +1,45 @@
 package br.com.navigationapp2
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.fragment.navArgs
+import br.com.navigationapp2.databinding.FragmentLoginBinding
+import br.com.navigationapp2.databinding.FragmentWelcomeBinding
 
 class WelcomeFragment: Fragment(R.layout.fragment_welcome) {
+
+    private var _binding: FragmentWelcomeBinding? = null
+    private val binding get() = _binding!!
+
+    private val args: WelcomeFragmentArgs by navArgs()
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentWelcomeBinding.inflate(inflater, container, false)
+        val view = binding.root
+        return view
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.usernameTextView.text = args.username
+        binding.passwordTextView.text = args.password
+        binding.backButton.setOnClickListener {
+            val action = WelcomeFragmentDirections.actionWelcomeFragmentToHomeFragment()
+            findNavController().navigate(action)
+        }
+
+    }
 }
